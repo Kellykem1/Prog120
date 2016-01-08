6610f98434ca7d64fa1c21c28116b44c4616b6cf
kellykem <kellykem@bellevuecollege.edu>
2016-01-08 10:53:28 -0800

    push code first time

---
+++ Banking/Banking/Program.cs
@@ -0,0 +1,74 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // static variable names are referenced by the class name
+            Console.WriteLine("At the beginning, BankAccounts has a value of: {0}", BankAccount.NumberAccounts);
+
+            Console.WriteLine();
+            Console.WriteLine("First we create a  new empty account");
+
+            BankAccount firstBankAccount = new BankAccount(); // by calling the simple constructor
+
+            Console.WriteLine("Now, BankAccounts has a value of: {0}", BankAccount.NumberAccounts);
+
+            Console.WriteLine();
+            Console.WriteLine("Second, we create a new account with values"); // by calling the other constructor
+
+            Console.Write("Enter first name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Enter last name: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Enter first deposit: ");
+            decimal balance = Convert.ToInt32(Console.ReadLine());
+
+            BankAccount secondBankAccount = new BankAccount(lastName, firstName, balance);
+            Console.WriteLine();
+
+            Console.WriteLine("Now BankAccounts has a value of: {0}", BankAccount.NumberAccounts);
+            Console.WriteLine();
+
+            Console.WriteLine("Now we will display values of both accounts");
+
+            Display(firstBankAccount, secondBankAccount);
+            Console.WriteLine();
+
+
+            Console.WriteLine("Next we will add information to first account using the props and method.");
+            Console.Write("Enter a first name: ");
+            firstBankAccount.OwnerFirstName = Console.ReadLine();
+            Console.Write("Enter a last name: ");
+            firstBankAccount.OwnerLastName = Console.ReadLine();
+            Console.Write("Enter a deposit amount: ");
+            firstBankAccount.Deposit(Convert.ToDecimal(Console.ReadLine()));
+
+            Console.WriteLine();
+            Console.WriteLine("Now we will display values of both accounts again");
+            Display(firstBankAccount, secondBankAccount);
+
+            Console.WriteLine();
+            Console.WriteLine("Now BankAccounts has a value of: {0}", BankAccount.NumberAccounts);
+
+
+            Console.ReadLine();
+        }
+
+        private static void Display(BankAccount aBankAccount, BankAccount bBankAccount)
+        {
+            Console.WriteLine("The first bank account has these values:");
+            Console.WriteLine(aBankAccount.OwnerFullName + "  " + aBankAccount.Balance);
+            Console.WriteLine();
+            Console.WriteLine("The second bank account has these values:");
+            Console.WriteLine(bBankAccount.OwnerFullName + "  " + bBankAccount.Balance);
+        }
+    }
+}
---
+++ Banking/ClassLibrary1/BankAccount.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class BankAccount
+    {
+        // class _fields
+        private string _myOwnerLastName;
+        private string _myOwnerFirstName;
+        private decimal _myBalance;
+        // make one static class variable
+        private static int _accountCount = 0;
+
+        #region "Constructors"
+
+        /// Default constructor - no parameters
+        public BankAccount()
+        {
+            _myBalance = 0;
+            _myOwnerFirstName = "not assigned";
+            _myOwnerLastName = "not assigned";
+            _accountCount = _accountCount + 1;
+        }
+
+
+        /// parameterized constructor
+        public BankAccount(string initialLastName, string initialFirstName, decimal initialBalance)
+        {
+            _myBalance = initialBalance;
+            _myOwnerFirstName = initialFirstName;
+            _myOwnerLastName = initialLastName;
+            _accountCount = _accountCount + 1;
+        }
+
+        #endregion
+
+        #region "Properties“         //public read only property
+        public static decimal NumberAccounts
+        {
+            get
+            {
+                return _accountCount;
+            }
+        }
+
+        // public property
+        public string OwnerLastName
+        {
+            get
+            {
+                return _myOwnerLastName;
+            }
+            set
+            {
+                _myOwnerLastName = value;
+            }
+        }
+
+        // public property
+        public string OwnerFirstName
+        {
+            get
+            {
+                return _myOwnerFirstName;
+            }
+            set
+            {
+                _myOwnerFirstName = value;
+            }
+        }
+        //public read only property
+        public string OwnerFullName
+        {
+            get
+            {
+                return _myOwnerLastName + ", " + _myOwnerFirstName;
+            }
+        }
+
+        //public read only property
+        public decimal Balance
+        {
+            get
+            {
+                return _myBalance;
+            }
+        }
+
+
+        #endregion
+
+        #region "Methods"
+
+        // public method
+        // <param name="amount">amount to deposit in account</param>
+        public void Deposit(decimal amount)
+        {
+            // code here would check a zillion things before doing the next line
+            _myBalance = _myBalance + amount;
+        }
+
+        #endregion
+
+
+    }
+}
